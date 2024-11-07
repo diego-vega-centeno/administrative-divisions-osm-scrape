3af3996cc89c9f6058f6088f592d682582c8aec1
diego vega <da-vce@hotmail.com>
2024-11-07 18:37:28 -0500

    Update filters and test

--- tools/filters.py
+++ tools/filters.py
@@ -6,16 +6,38 @@ def test(type):
     match type:
         case 'leaksFromOtherCountries':
             return leaksFromOtherCountries
-def testSanitize(raw):
+        
+def testSanitize(elements):
+    
     # some elements have missing name
-    miss = list(filter(lambda x: testFunc('missingName')(x), raw['elements']))
+    miss = list(filter(lambda x: testFunc('missingName')(x), elements))
     print('missing name: ', len(miss))
 
     # duplicates elements happens with are method where polygon intersect other countries areas
-    dup = list(findDuplicates([ele['id'] for ele in raw['elements']]))
+    dup = list(findDuplicates([ele['id'] for ele in elements]))
     print('duplicate elements: ', len(dup))
 
-    return [miss, dup]
+    # relations from other countries
+    cntrIso = [
+        ele['tags']['ISO3166-1'] 
+        for ele in elements 
+        if ele['tags']['admin_level'] == '2'
+    ][0]
+
+    relLeaks = [
+        ele for ele in elements 
+        # if not matchISO(ele["tags"].get("ISO3166-2", ""), cntrIso)
+        if not matchISO(next((
+            ele['tags'][key] 
+            for key 
+            in ["ISO3166-2", "ref:nuts", 'ref:nuts:2', 'ref:nuts:3', 'ref'] 
+            if key in ele['tags'] 
+        ), ''), cntrIso)
+    ]
+
+    print('relations from other countries: ', len(relLeaks))
+
+    return [miss, dup, relLeaks]
 
 def testFunc(type):
     match type:
