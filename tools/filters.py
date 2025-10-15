bc12e51f92de68ecfafb1338be31d8382841a01c
diego vega <da-vce@hotmail.com>
2025-10-15 16:55:04 -0500

    Move osm query data to 'data/osm countries' Add osm test to filters Process new elements with these filters

--- tools/filters.py
+++ tools/filters.py
@@ -1,5 +1,8 @@
 import re
 from toolsGeneral.main import findDuplicates
+from toolsOSM.overpass import getCenterNodeInsideParent
+import pandas as pd
+import time
 
 def test(type):
 
@@ -7,54 +10,88 @@ def test(type):
         case 'leaksFromOtherCountries':
             return leaksFromOtherCountries
         
-def testSanitize(elements):
+def testOSMElements(df):
     
+    cntrRow = df['tags.ISO3166-1'].notna()
+    cntrISO = df[cntrRow].iloc[0]['tags.ISO3166-1']
+    cntrName = df[cntrRow].iloc[0]['tags.name']
+    print(cntrName, '-', df[cntrRow].iloc[0]['id'])
+
     # some elements have missing name
-    miss = list(filter(lambda x: testFunc('missingName')(x), elements))
-    print('missing name: ', len(miss))
+    miss = df[df['tags.name'].isna()]
+    print('\tmissing names: ', len(miss))
 
     # duplicates elements happens with are method where polygon intersect other countries areas
-    dup = list(findDuplicates([ele['id'] for ele in elements]))
-    print('duplicate elements: ', len(dup))
+    dup = df[df.duplicated('id', keep=False)]
+    dupTestRes = []
+    print('\tduplicate elements by id: ', len(dup))
+    if(len(dup) > 0):
+      print('\t\ttesting osm center: ', len(dup))
+      # dupTestRes = [getCenterNodeInsideParent(c,p) for c,p in zip(dupToTest['id'],dupToTest['tags.parent_id'])]
+      for groupId, group in dup.groupby('id'):
+        for _, row in group.iterrows():
+          res = [
+            row['id'],
+            row['tags.parent_id'],
+            getCenterNodeInsideParent(row['id'],row['tags.parent_id'])
+          ]
+          dupTestRes.append(res)
+          print(f'\t\t* {[row['id'], row['tags.parent_id']]}: {res[2]['status']}')
+      # dupTestRes = list(filter(lambda x: x[2]['status']=='ok' and len(x[2]['elements'])>0, dupTestRes))
+
 
     # relations from other countries
-    cntrIso = [
-        ele['tags']['ISO3166-1'] 
-        for ele in elements 
-        if ele['tags']['admin_level'] == '2'
-    ][0]
+    checkTags = ["tags.is_in:country", "tags.ISO3166-2", "tags.ref:nuts", 'tags.ref:nuts:2', 'tags.ref:nuts:3']
+    leakRows = []
+    isInCountry = {}
 
-    relLeaks = [
-        ele for ele in elements 
-        # if not matchISO(ele["tags"].get("ISO3166-2", ""), cntrIso)
-        if not matchISO(next((
-            ele['tags'][key] 
-            for key 
-            in ["ISO3166-2", "ref:nuts", 'ref:nuts:2', 'ref:nuts:3', 'ref'] 
-            if key in ele['tags'] 
-        ), ''), cntrIso)
-    ]
+    for idx, row in df.iterrows():
+      osmID = str(row.get('id'))
+      parentID = row.get('tags.parent_id')
+      foundTag = False
+      for tag in checkTags:
+        val = row.get(tag)
+        if pd.isna(val):
+          continue
 
-    print('relations from other countries: ', len(relLeaks))
+        foundTag = True
 
-    return [miss, dup, relLeaks]
+        # Handle explicit country tag
+        if tag == "tags.is_in:country":
+          if val.strip().lower() == cntrName.strip().lower():
+            isInCountry[osmID] = True
+          else:
+            # not the same country -> verify with parent
+            if pd.notna(parentID) and isInCountry.get(parentID):
+              isInCountry[osmID] = True
+            else:
+              leakRows.append(row)
+          break # no need to check other tags
+        # Handle ISO-style tags
+        elif not checkISO(val, cntrISO):
+          leakRows.append(row)
+          break
+        else:
+          isInCountry[osmID] = True
+          break
+      if not foundTag and pd.notna(parentID) and isInCountry.get(parentID):
+        isInCountry[osmID] = True
+
+
+    print('\trelations from other countries: ', len(leakRows))
+
+    return {'missing.name':miss, 'duplicate.id':dup, 'duplicate.centerTest':dupTestRes, 'leak':leakRows}
 
 def testFunc(type):
     match type:
         case 'missingName': 
             return lambda x: x['tags'].get("name", '') == ''
-        case 'matchISO': 
-            return matchISO  
 
 
-def matchISO(code, cntrCode):
-
+def checkISO(code, cntrCode):
     if code == '': return True
-
     iso1 = re.search(r"^([A-Z]{2})", code)
-
     iso1 = iso1.group(0) if iso1 else ''
-
     return iso1 == cntrCode
 
 def leaksFromOtherCountries(data, cntrId):
