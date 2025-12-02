1986aea2ba6da07486b5378e767ae393cde62a33
diego vega <da-vce@hotmail.com>
2025-12-02 18:04:50 -0500

    Refactor update_process_state to update 'status' and 'last_run' Delete backblaze b2 if it exists

--- src/tools/upload_and_commit.py
+++ src/tools/upload_and_commit.py
@@ -3,6 +3,7 @@ import toolsGeneral.main as tgm
 from pathlib import Path
 import os
 import datetime
+from botocore.exceptions import ClientError
 
 def upload_dir_files_to_backblaze(dir:Path, config):
 
@@ -12,6 +13,15 @@ def upload_dir_files_to_backblaze(dir:Path, config):
 
     for file in dir.rglob("*"):
         if file.is_file():
+            # delete file if exists
+            try:
+                s3.delete_object(Bucket=os.environ["B2_BUCKET_NAME"], Key=str(file.relative_to(root)))
+            except ClientError as e:
+                # Ignore error if file doesn't exist
+                if e.response['Error']['Code'] != "NoSuchKey":
+                    logger.error(f"Failed delete {file}: {e}")
+                    raise # raise same exception
+            # upload file
             try:
                 s3.upload_file(
                     str(file), 
@@ -35,10 +45,10 @@ def commit_file(file:Path, commit_msg, logger):
     except Exception as e:
         logger.error(f"Failed to commit {file.name}: {e}")
 
-def update_process_state(process_state, country, process_type, process_result):
+def update_process_state(process_state, country, process_type, process_status):
     process_state.setdefault(country, {
         key: {"status": "pending", "last_run": None, "error": None} for key in 
         ["scrape", "clean", "test_basic", "test_first_level", "test_duplicates", "fix"]
     })
-    process_state[country][process_type] = process_result
-    process_state[country][process_type] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    process_state[country][process_type]['status'] = process_status
+    process_state[country][process_type]['last_run'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
