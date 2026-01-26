ae0e96c57d035217fa410a35213ae6fc572a3fd1
cocobongo <da-vce@hotmail.com>
2026-01-25 21:36:17 -0500

    feat: Download to _new.json for review

--- scripts/pull_from_B2.py
+++ scripts/pull_from_B2.py
@@ -12,9 +12,9 @@ ROOT = Path(__file__).resolve().parents[1]
 DATA_DIR = ROOT / "data"
 logger = tgl.initiate_logger('logger')
 load_dotenv()
-process_state_file = DATA_DIR / "process_state.json"
-dups_test_state_file = DATA_DIR / "dups_test_state.json"
-first_level_test_state_file = DATA_DIR / "first_level_test_state.json"
+process_state_file = Path(DATA_DIR / "process_state.json")
+dups_test_state_file = Path(DATA_DIR / "dups_test_state.json")
+first_level_test_state_file = Path(DATA_DIR / "first_level_test_state.json")
 
 #* setup b2
 session = boto3.session.Session()
@@ -26,6 +26,6 @@ s3 = session.client(
 )
 
 #* download files
-tsm.download_file_from_bucket(os.environ["B2_BUCKET_NAME"], process_state_file.relative_to(ROOT), s3, process_state_file, logger)
-tsm.download_file_from_bucket(os.environ["B2_BUCKET_NAME"], dups_test_state_file.relative_to(ROOT), s3, dups_test_state_file, logger)
-tsm.download_file_from_bucket(os.environ["B2_BUCKET_NAME"], first_level_test_state_file.relative_to(ROOT), s3, first_level_test_state_file, logger)
+tsm.download_file_from_bucket(os.environ["B2_BUCKET_NAME"], process_state_file.relative_to(ROOT), s3, process_state_file.with_name("process_state_new.json"), logger)
+tsm.download_file_from_bucket(os.environ["B2_BUCKET_NAME"], dups_test_state_file.relative_to(ROOT), s3, dups_test_state_file.with_name("dups_test_state_new.json"), logger)
+tsm.download_file_from_bucket(os.environ["B2_BUCKET_NAME"], first_level_test_state_file.relative_to(ROOT), s3, first_level_test_state_file.with_name("first_level_test_state_new.json"), logger)
